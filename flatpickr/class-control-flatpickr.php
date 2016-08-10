7c91b36c09f5e1a87c98841eb1d580c4a9cb540b
m-e-h <info@martyhelmick.com>
2016-08-10 19:50:13 -0400

    fix flatpicker. add some options

--- flatpickr/class-control-flatpickr.php
+++ flatpickr/class-control-flatpickr.php
@@ -11,26 +11,46 @@ class ButterBean_Control_FlatPickr extends ButterBean_Control {
 
 	public $show_time = true;
 
+	public $date_format = 'Y-m-d';
+
+	public $min_date = '';
+
+	public $max_date = '';
+
 	public function to_json() {
 		parent::to_json();
 
 		$this->json['show_time'] = $this->show_time;
 
+		$this->json['date_format'] = $this->date_format;
+
+		$this->json['min_date'] = $this->min_date;
+
+		$this->json['max_date'] = $this->max_date;
+
 		$this->json['value'] = esc_html( $this->get_value() );
 	}
 
 	public function get_template() {
-		$field_name = get_field_name();
+
 		wp_enqueue_style( 'flatpickr' );
 		wp_enqueue_script( 'flatpickr' );
-		wp_add_inline_script ( 'flatpickr', "document.getElementById('{$field_name}').flatpickr();" );
+		wp_add_inline_script ( 'flatpickr', $this->bbs_get_flatpickr_script() );
 		?>
 
 		<div class="row">
 			<p class="flatpickr input-group">
 				<span class="butterbean-label">{{ data.label }}</span>
-				<input type="text" id="{{ data.field_name }}" class="flatpickr-input" placeholder="Date" value="{{ data.value }}" name="{{ data.field_name }}" data-enable-time=true>
+				<input type="text" id="{{ data.field_name }}" class="flatpickr-input" placeholder="Date" value="{{ data.value }}" name="{{ data.field_name }}" data-enable-time="{{ data.show_time }}" data-min-date="{{ data.min_date }}" data-date-format="{{ data.date_format }}">
 			</p>
 		</div>
 		<?php }
+
+
+	/**
+	 * @return string
+	 */
+	public function bbs_get_flatpickr_script() {
+		return "document.querySelectorAll('.flatpickr-input').flatpickr();";
+	}
 }
--- flatpickr/metaboxes.php
+++ flatpickr/metaboxes.php
@@ -45,25 +45,60 @@ if ( ! class_exists( 'FlatPickr_Meta' ) ) {
 				)
 			);
 
+			$manager->register_control(
+				'event_allday',
+				array(
+					'type'        => 'checkbox',
+					'section'     => 'bbs_flatpickr_fields',
+					'label'       => 'All day event',
+					'description' => 'Example description.'
+				)
+			);
+
 			$dir_path = bbs_get_dir_path();
 			require_once $dir_path . 'flatpickr/class-control-flatpickr.php';
 
 			$manager->register_control(
 				new ButterBean_Control_FlatPickr(
 					$manager,
-					'bbs_flatpickr',
+					'event_start',
 					array(
 						'type'        => 'flatpickr',
 						'section'     => 'bbs_flatpickr_fields',
-						'label'       => 'Event Date',
+						'label'       => 'Start Date',
 						'description' => 'Example description.'
 					)
 				)
 			);
 
+			$manager->register_control(
+				new ButterBean_Control_FlatPickr(
+					$manager,
+					'event_end',
+					array(
+						'type'        	=> 'flatpickr',
+						'section'     	=> 'bbs_flatpickr_fields',
+						'label'       	=> 'End Date',
+						'date_format'	=> 'm/d/Y',
+						'show_time'     => '',
+						'min_date'     	=> '2016-08-12'
+					)
+				)
+			);
+
 			$manager->register_setting(
-				'bbs_flatpickr',
+				'event_start',
 				array( 'sanitize_callback' => 'wp_filter_nohtml_kses' )
+			);
+
+			$manager->register_setting(
+				'event_end',
+				array( 'sanitize_callback' => 'wp_filter_nohtml_kses' )
+			);
+
+			$manager->register_setting(
+				'event_allday',
+				array( 'sanitize_callback' => 'butterbean_validate_boolean' )
 			);
 		}
 		/**
